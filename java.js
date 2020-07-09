b32921e3089753e010a9c699811a660f6065728e
Bryce Cartwright <Cartwright@Cartwrights-Air-2.local>
2020-07-09 19:38:29 -0400

    new

--- java.js
+++ java.js
@@ -2,7 +2,6 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
-app.use("/", express.static("assets"));
 
 app.get("/index", function (req, res) {
   res.render("index");
