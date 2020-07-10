295cc2eccf8b734c1223c8e9b3c75fc59875fc74
Bryce Cartwright <Cartwright@Cartwrights-Air-2.local>
2020-07-10 11:18:47 -0400

    update

--- java.js
+++ java.js
@@ -1,14 +1,23 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set("view engine", "ejs");
+app.use("/assets", express.static("public"));
 
-app.get("/index", function (req, res) {
-  res.render("index");
+app.get("/home", function (req, res) {
+  res.render("home");
 });
-app.get("/blank", function (req, res) {
-  res.render("blank");
+app.get("/contact", function (req, res) {
+  res.render("contact", { qs: req.query });
 });
+
+app.post("/contact", urlencodedParser, function (req, res) {
+  console.log(req.body);
+  res.render("contact-success", { data: req.body });
+});
+
 app.get("/profile/:id", function (req, res) {
   var data = {
     age: 24,
